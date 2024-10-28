133e8c676981fa546ce5c79882dd903d36f90395
amir811537 <amirhossain.bc75@gmail.com>
2024-10-28 10:21:59 +0600

    added new data

--- public/categoryObject.js
+++ public/categoryObject.js
@@ -5,6 +5,11 @@ export  const categoryItems = [
             "https://i.ibb.co.com/hXjw8nw/116145.png",
     },
     {
+        title: "eyeglasses frames",
+        imageurl:
+            "https://i.ibb.co.com/1s2jJrX/DSF7525-Photoroom-1500x1500-removebg-preview.png",
+    },
+    {
         title: "Classic watches",
         imageurl:
             "https://i.ibb.co.com/mRn1ptK/wristwatch-line-icon-isolated-vector-600nw-2468699755.webp",
